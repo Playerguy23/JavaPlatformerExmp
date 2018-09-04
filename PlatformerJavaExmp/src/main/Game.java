58d9341e7bf95b1aa87463099396c272e9027fa8
Playerguy23 <maa1kivi@gmail.com>
2018-09-04 20:29:04 +0300

    Add files via upload

---
+++ PlatformerJavaExmp/src/main/Game.java
@@ -0,0 +1,22 @@
+package main;
+
+import main.display.Display;
+
+public class Game implements Runnable{
+
+	private String title;
+	private int width;
+	private int height;
+	
+	private Display display;
+	
+	public Game(String title, int width, int height) {
+		
+	}
+	
+	@Override
+	public void run() {
+		
+	}
+	
+}
---
+++ PlatformerJavaExmp/src/main/Launcher.java
@@ -0,0 +1,9 @@
+package main;
+
+import main.display.Display;
+
+public class Launcher {
+	public static void main(String[] args) {
+		new Display("Platformer", 600, 400);
+	}
+}
